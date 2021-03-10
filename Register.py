f95cca53ab77c7f8e14215e3673fbf0b91ffc8ec
waqarkaleemkhan <waqar.musa777@gmail.com>
2021-03-10 16:34:16 +0500

    validation for the registration is done

--- Register.py
+++ Register.py
@@ -1,6 +1,7 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk,messagebox
 from PIL import Image,ImageTk
+import psycopg2
 class Register():
     def __init__(self,root):
         self.root=root
@@ -22,37 +23,60 @@ class Register():
 
         # input fields
         first_name=Label(frame1,text="First Name",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=50,y=100)
-        txt_fname=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=50,y=130,width=250)
+        self.txt_fname=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_fname.place(x=50,y=130,width=250)
 
         last_name=Label(frame1,text="Last Name",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=370,y=100)
-        txt_lname=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=370,y=130,width=250)
+        self.txt_lname=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_lname.place(x=370,y=130,width=250)
 
         contact=Label(frame1,text="Contact",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=50,y=170)
-        txt_contact=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=50,y=200,width=250)
+        self.txt_contact=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_contact.place(x=50,y=200,width=250)
 
         email=Label(frame1,text="Email",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=370,y=170)
-        txt_email=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=370,y=200,width=250)
+        self.txt_email=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_email.place(x=370,y=200,width=250)
 
         question=Label(frame1,text="Securtiy Qustion",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=50,y=240)
-        cmb_quest=ttk.Combobox(frame1,font=("times new roman",13),state="readonly",justify=CENTER)
-        cmb_quest['values']=("Select","Your first pet name","Your Birth Place","Your Best Friend Name")
-        cmb_quest.place(x=50,y=270,width=250)
-        cmb_quest.current(0)
+        self.cmb_quest=ttk.Combobox(frame1,font=("times new roman",13),state="readonly",justify=CENTER)
+        self.cmb_quest['values']=("Select","Your first pet name","Your Birth Place","Your Best Friend Name")
+        self.cmb_quest.place(x=50,y=270,width=250)
+        self.cmb_quest.current(0)
 
         answer=Label(frame1,text="Answer",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=370,y=240)
-        txt_answer=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=370,y=270,width=250)
+        self.txt_answer=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_answer.place(x=370,y=270,width=250)
 
         password=Label(frame1,text="Password",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=50,y=310)
-        txt_password=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=50,y=340,width=250)
+        self.txt_password=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_password.place(x=50,y=340,width=250)
 
         conform_password=Label(frame1,text="Conform Password",font=("times new roman",15,"bold"),bg="white",fg="gray").place(x=370,y=310)
-        txt_conform_password=Entry(frame1,font=("times new roman",15),bg="light gray").place(x=370,y=340,width=250)
+        self.txt_conform_password=Entry(frame1,font=("times new roman",15),bg="light gray")
+        self.txt_conform_password.place(x=370,y=340,width=250)
 
-
-        chk=Checkbutton(frame1,text="I Agree The Terms and Conditions",onvalue=1,offvalue=0,font=("Times new roman",12),bg="white").place(x=50,y=380)
-
-        btn=Button(frame1,text="Register",font=("times new roman",15),bg="purple1").place(x=50,y=420,width=250)
+        self.var_chk=IntVar()
+        self.chk=Checkbutton(frame1,text="I Agree The Terms and Conditions",variable=self.var_chk,onvalue=1,offvalue=0,font=("Times new roman",12),bg="white").place(x=50,y=380)
+        
+        btn_register=Button(frame1,text="Register",command=self.register_data, font=("times new roman",15),bg="purple1").place(x=50,y=420,width=250)
+        
         btn_login=Button(self.root,text="Login",font=("times new roman",15),bg="purple1",cursor="hand2").place(x=150,y=510,width=250)
+
+
+    def register_data(self):
+        if self.txt_fname.get()=="" or self.txt_lname.get()=="" or self.txt_email.get()=="" or self.txt_contact.get()=="" or self.txt_conform_password.get()=="" or self.cmb_quest.get()=="Select" or self.txt_answer.get()=="" or self.txt_password.get()=="" or self.txt_conform_password.get()=="":
+            messagebox.showerror("Error","All Fields are Required",parent=self.root)
+        elif self.txt_password.get()!=self.txt_conform_password.get():
+            messagebox.showerror("Error","Password is not same",parent=self.root)
+        elif self.var_chk.get()==0:
+            messagebox.showerror("Error","Agree to out terms and conditions",parent=self.root)
+        else:
+            messagebox.showinfo("Success","Register Successfull",parent=self.root)
+            
+        
+
+
 root=Tk()
 obj=Register(root)
 root.mainloop()
